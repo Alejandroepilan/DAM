9082293fa21ff9492d62c5277808d6a223281e69
Alejandroepilan <alejandroepilan@gmail.com>
2023-09-25 19:37:08 +0200

    semana 1 programacion

---
+++ Programacion/Semana1/ProyectoJava2/src/proyectojava2/ProyectoJava2.java
@@ -0,0 +1,15 @@
+package proyectojava2;
+
+public class ProyectoJava2 {
+
+    public static void main(String[] args) {
+        
+        byte edad = 42;
+        String nombre = "Alejandro Epila";
+        
+        System.out.println("Mi edad es de " + edad + " años y mi nombre es " + nombre);
+        System.out.printf("Mi edad es de %d años y mi nombre es %s \n", edad, nombre);
+        
+    }
+    
+}
---
+++ Programacion/Semana1/ProyectoJava3/src/proyectojava3/ProyectoJava3.java
@@ -0,0 +1,38 @@
+
+package proyectojava3;
+
+public class ProyectoJava3 {
+
+    public static void main(String[] args) {
+        
+        float operador1 = 4;
+        float operador2 = 3;
+        
+        float suma = operador1 + operador2;
+        System.out.println("La suma es "+suma);
+        
+        float resta = operador1 - operador2;
+        System.out.println("La resta es "+resta);
+        
+        float multiplicacion = operador1 * operador2;
+        System.out.println("La multiplicacion es "+multiplicacion);
+        
+        double division = operador1 / operador2;
+        System.out.println("La division es "+division);
+        
+        
+        boolean igualdad = operador1 == operador2;
+        System.out.println("La comparacion es "+igualdad);
+        
+        boolean noigualdad = operador1 != operador2;
+        System.out.println("La comparacion es "+noigualdad);
+        
+        boolean menorque = operador1 < operador2;
+        System.out.println("La comparacion es "+menorque);
+        
+        boolean mayorque = operador1 < operador2;
+        System.out.println("La comparacion es "+mayorque);
+        
+    }
+    
+}
